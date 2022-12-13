75da337e687d0644fd4c72d3e1554a7d79c1ad5b
tech@turbo.ua <tech@turbo.ua>
2022-12-13 08:26:37 +0000

    incremental

--- models/staging/rawdata/base_products.sql
+++ models/staging/rawdata/base_products.sql
@@ -1,5 +1,17 @@
-SELECT order_date, clien_id, product_name, product_category, platform, payment_method, status, count(distinct d.transaction_id) as orders, sum(price * p.quantity) as item_revenue, p.quantity, sum(margin) as margin 
-FROM {{ ref('stg_products') }} p
-LEFT JOIN {{ ref('base_deals') }} d on p.transaction_id=d.transaction_id
-group by 1,2,3,4,5,6,7,10
+select
+    order_date,
+    p.transaction_id,
+    clien_id,
+    product_name,
+    product_category,
+    platform,
+    payment_method,
+    status,
+    count(distinct d.transaction_id) as orders,
+    sum(price * p.quantity) as item_revenue,
+    p.quantity,
+    sum(margin) as margin
+from {{ ref("stg_products") }} p
+left join {{ ref("base_deals") }} d on p.transaction_id = d.transaction_id
+group by 1, 2, 3, 4, 5, 6, 7, 8, 11
 order by count(d.transaction_id) desc
--- models/staging/rawdata/ga4_raw__sources.sql
+++ models/staging/rawdata/ga4_raw__sources.sql
@@ -6,7 +6,7 @@ ga4 as(
     select user_pseudo_id, source, medium, campaign from {{ ref('base_ga4__ecommerce') }}
 )
 
-select order_date,  product_name, product_category, source, medium, campaign, platform, payment_method, status, item_revenue, quantity, margin 
+select order_date, transaction_id,  clien_id,  product_name, product_category, source, medium, campaign, platform, payment_method, status, item_revenue, quantity, margin 
 from {{ ref('base_products') }} b left join  {{ ref('base_ga4__ecommerce') }} g
 on b.clien_id=g.user_pseudo_id
-group by 1,2,3,4,5,6,7,8,9,10,11,12
+group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14
