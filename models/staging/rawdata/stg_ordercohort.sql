1ec0b521a63797947b83292d3ea9c1b749a3056d
tech@turbo.ua <tech@turbo.ua>
2023-02-16 14:22:02 +0000

    cohort tables updated

---
+++ models/staging/rawdata/stg_ordercohort.sql
@@ -0,0 +1,88 @@
+{{ config(materialized="table") }}
+
+select
+    extract(year from b.date) as cohort_year,
+    format_date('%B', b.date) as month,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 0 then o.transaction_id
+        end
+    ) as month_0,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 1 then o.transaction_id
+        end
+    ) as month_1,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 2 then o.transaction_id
+        end
+    ) as month_2,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 3 then o.transaction_id
+        end
+    ) as month_3,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 4 then o.transaction_id
+        end
+    ) as month_4,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 5 then o.transaction_id
+        end
+    ) as month_5,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 6 then o.transaction_id
+        end
+    ) as month_6,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 7 then o.transaction_id
+        end
+    ) as month_7,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 8 then o.transaction_id
+        end
+    ) as month_8,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 9 then o.transaction_id
+        end
+    ) as month_9,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 10 then o.transaction_id
+        end
+    ) as month_10,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 11 then o.transaction_id
+        end
+    ) as month_11,
+    count(
+        distinct case
+            when date_diff(o.date, b.date, month) = 12 then o.transaction_id
+        end
+    ) as month_12
+
+from {{ source("raw_db", "raw_deals") }} o
+join
+    (
+        select user_id, transaction_id, min(date) as date
+        from {{ source("raw_db", "raw_deals") }}
+        group by user_id, 2
+    ) b
+    on o.user_id = b.user_id
+join
+    (
+        select min(date) as cohort_month, user_id, transaction_id
+        from {{ source("raw_db", "raw_deals") }}
+        group by user_id, 3
+    ) bc
+    on b.user_id = bc.user_id
+group by 1, 2
+order by 1, 2
